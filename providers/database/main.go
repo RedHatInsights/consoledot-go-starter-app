9c3119327f078eca07cfda29c27140d79815f81a
Adam Drew <addrew@redhat.com>
2023-06-10 12:09:53 -0400

    Database provider

---
+++ providers/database/main.go
@@ -0,0 +1,20 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/RedHatInsights/consoledot-go-starter-app/config"
+	"github.com/jackc/pgx/v5"
+)
+
+func Connect(conf *config.Config) (*pgx.Conn, error) {
+	conn, err := pgx.Connect(context.Background(), conf.DatabaseConnectionString())
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		return nil, err
+	}
+	defer conn.Close(context.Background())
+	return conn, nil
+}
