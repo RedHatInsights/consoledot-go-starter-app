5ed1c2d0ef9411b4a922885502ea08d06a64f7a4
Adam Drew <addrew@redhat.com>
2023-06-10 11:20:23 -0400

    Getting config working with clowder config and local environment

--- config/main.go
+++ config/main.go
@@ -1,14 +1,68 @@
 package config
 
 import (
+	"fmt"
+	"os"
+	"strconv"
+
 	clowder "github.com/redhatinsights/app-common-go/pkg/api/v1"
+
+	"github.com/joho/godotenv"
 )
 
-var AppConfig *clowder.AppConfig
-
-func init() {
-	if clowder.IsClowderEnabled() {
-		AppConfig = clowder.LoadedConfig
+func Load() *Config {
+	c := Config{
+		EnvVars:   make(map[string]string),
+		AppConfig: &clowder.AppConfig{},
 	}
-	AppConfig.MetricsPort = 8080
+	c.Load()
+	return &c
+}
+
+type Config struct {
+	AppConfig *clowder.AppConfig
+	EnvVars   map[string]string
+}
+
+func (c *Config) RouterBindAddress() string {
+	host := os.Getenv("HOST") + ":"
+
+	//Append AppConfig.PublicPort to host
+	host += fmt.Sprint(*c.AppConfig.PublicPort)
+
+	return host
+}
+
+func (c *Config) Load() {
+	c.loadEnvVars()
+	if clowder.IsClowderEnabled() {
+		c.AppConfig = clowder.LoadedConfig
+	} else {
+		c.setAppConfigFromEnvVars()
+	}
+}
+
+func (c *Config) loadEnvVars() {
+	err := godotenv.Load("local.env")
+	if err != nil {
+		// ... handle error
+		//log.Fatalf("Some error occured. Err: %s", err)
+		panic(err)
+	}
+}
+
+func (c *Config) setAppConfigFromEnvVars() {
+	//Set the AppConfig public port from the publicPort const
+	c.AppConfig.PublicPort = c.getEnvVarIntPtr("PUBLIC_PORT")
+}
+
+func (c *Config) getEnvVarIntPtr(envVar string) *int {
+	// string to int
+	i, err := strconv.Atoi(os.Getenv(envVar))
+	if err != nil {
+		// ... handle error
+		// log.Fatalf("Some error occured. Err: %s", err)
+		panic(err)
+	}
+	return &i
 }
