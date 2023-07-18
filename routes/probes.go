7fcc3e1e567e569a251eb7cd2c182fe3a6851293
Adam Drew <addrew@redhat.com>
2023-07-18 08:09:25 -0400

    Add some commentage about good probes

--- routes/probes.go
+++ routes/probes.go
@@ -19,6 +19,10 @@ func setupProbes(router *gin.Engine, apiPath string) {
 // @Success      200  {object}  map[string]any
 // @Router       /readyz [get]
 func readinessProbe(context *gin.Context) {
+	// Your probes should not just return OK, but should also return useful information about the state of your
+	// application. Reporting on the state of dependencies is a good example of this. However, this must be
+	// balanced against the impact of the probes being hit frequently by the kubernetes API. The right
+	// balance will depend on your application and your environment.
 	context.JSON(http.StatusOK, gin.H{
 		"ready": "OK",
 	})
@@ -32,6 +36,10 @@ func readinessProbe(context *gin.Context) {
 // @Success      200  {object}  map[string]any
 // @Router       /healthz [get]
 func livelinessProbe(context *gin.Context) {
+	// Your probes should not just return OK, but should also return useful information about the state of your
+	// application. Reporting on the state of dependencies is a good example of this. However, this must be
+	// balanced against the impact of the probes being hit frequently by the kubernetes API. The right
+	// balance will depend on your application and your environment.
 	context.JSON(http.StatusOK, gin.H{
 		"alive": "OK",
 	})
