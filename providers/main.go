0d8b6e8e26ef20b4008a7567a9989af8c0ff5404
Adam Drew <addrew@redhat.com>
2023-08-22 09:04:37 -0400

    Made the database conditionality work the way it was supposed to. Refine the makefile a bit

--- providers/main.go
+++ providers/main.go
@@ -1,8 +1,6 @@
 package providers
 
 import (
-	"os"
-
 	"github.com/RedHatInsights/consoledot-go-starter-app/config"
 	"github.com/RedHatInsights/consoledot-go-starter-app/providers/database"
 	"github.com/rs/zerolog/log"
@@ -36,8 +34,7 @@ func Init(conf *config.Config) (Providers, func(Providers)) {
 func dbConnect(conf *config.Config) database.ConnectionPool {
 	connPool, err := database.Connect(conf)
 	if err != nil {
-		log.Error().Err(err).Msg("Error connecting to database. Exiting ...")
-		os.Exit(1)
+		log.Error().Err(err).Msg("Error connecting to database. ")
 	}
 	return connPool
 }
