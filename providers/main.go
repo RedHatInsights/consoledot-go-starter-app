ed078940d294797cb12f269f893f21c7c79377a6
Adam Drew <addrew@redhat.com>
2023-08-23 16:44:01 -0400

    Go back to original die on boot if no DB system.

--- providers/main.go
+++ providers/main.go
@@ -1,6 +1,8 @@
 package providers
 
 import (
+	"os"
+
 	"github.com/RedHatInsights/consoledot-go-starter-app/config"
 	"github.com/RedHatInsights/consoledot-go-starter-app/providers/database"
 	"github.com/rs/zerolog/log"
@@ -48,7 +50,8 @@ func Init(conf *config.Config) (Providers, func(Providers)) {
 func dbConnect(conf *config.Config) database.ConnectionPool {
 	connPool, err := database.Connect(conf)
 	if err != nil {
-		log.Error().Err(err).Msg("Error connecting to database. ")
+		log.Error().Err(err).Msg("Error connecting to database on startup. Exiting.")
+		os.Exit(1)
 	}
 	return connPool
 }
